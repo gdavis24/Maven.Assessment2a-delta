d21244cc470f314c2fae6d65edb0252cc392769b
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-02 10:31:18 -0500

    isEven and isOdd tests done

--- src/main/java/rocks/zipcode/assessment2/fundamentals/PredicateUtilities.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/PredicateUtilities.java
@@ -5,23 +5,40 @@ public class PredicateUtilities {
      * @param value - the value to be evaluated
      * @return true if `value` is a multiple of 2
      */
-    public static Boolean isEven(Integer value) {
-        return null;
+    public static Boolean isEven(Integer value)
+    {
+        if (value % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
      * @param value - the value to be evaluated
      * @return true if `value` is not a multiple of 2
      */
-    public static Boolean isOdd(Integer value) {
-        return null;
+    public static Boolean isOdd(Integer value)
+    {
+        if (value % 2 == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     /**
      * @param value - the value to be evaluated
      * @return true if `value` is a multiple of 3
      */
-    public static Boolean isMultipleOf3(Integer value) {
+    public static Boolean isMultipleOf3(Integer value)
+    {
         return null;
     }
 
@@ -31,7 +48,8 @@ public class PredicateUtilities {
      * @param multiple - the multiple to test `value` against
      * @return true if `value` is a multiple of `multiple`
      */
-    public static Boolean isMultipleOfN(Integer value, Integer multiple) {
+    public static Boolean isMultipleOfN(Integer value, Integer multiple)
+    {
         return null;
     }
 
@@ -39,7 +57,8 @@ public class PredicateUtilities {
      * @param string - the string to be evaluated
      * @return true if `string` starts with a capital letter
      */
-    public static Boolean startsWithCapitalLetter(String string) {
+    public static Boolean startsWithCapitalLetter(String string)
+    {
         return null;
     }
 }
